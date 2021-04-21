40b74c0d48bb60c467b51fd1633a10add2ab6592
Andrew Johnson <he184239@health.wa.gov.au>
2021-04-21 00:48:02 +0000

    Deleted .tmp

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var PBIFUN: IVisualPlugin;
-export default PBIFUN;
