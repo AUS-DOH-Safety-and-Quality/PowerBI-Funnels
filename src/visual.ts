533595275dc0079a71a2dc9f7ed32bc47ac02388
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-03-03 15:11:42 +0800

    Multip 100 handling

--- src/visual.ts
+++ src/visual.ts
@@ -247,7 +247,7 @@ export class Visual implements IVisual {
       yAxis = d3.axisLeft(this.viewModel.plotProperties.yScale).tickFormat(
         d => {
           return this.viewModel.inputData.percentLabels
-            ? (<number>d).toFixed(2) + "%"
+            ? (<number>d * (this.viewModel.inputData.multiplier === 100 ? 1 : this.viewModel.inputData.multiplier)).toFixed(2) + "%"
             : (<number>d).toFixed(2);
         }
       );
