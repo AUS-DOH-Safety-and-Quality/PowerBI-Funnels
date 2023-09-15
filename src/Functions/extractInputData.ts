0cd18c8b227cdd9a4b6c25cb62272ca3b6b32a66
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-09-15 16:47:30 +0300

    Display warning on data removal

--- src/Functions/extractInputData.ts
+++ src/Functions/extractInputData.ts
@@ -1,5 +1,5 @@
 import type powerbi from "powerbi-visuals-api";
-import { extractValues, checkValidInput, extractDataColumn, extractConditionalFormatting, rep } from "../Functions"
+import { extractValues, validateInputData, extractDataColumn, extractConditionalFormatting, rep } from "../Functions"
 import { type defaultSettingsType } from "../Classes"
 
 export type dataObject = {
@@ -12,6 +12,7 @@ export type dataObject = {
   percentLabels: boolean;
   categories: powerbi.DataViewCategoryColumn;
   scatter_formatting: defaultSettingsType["scatter"][];
+  warningMessage: string;
 }
 
 export default function extractInputData(inputView: powerbi.DataViewCategorical, inputSettings: defaultSettingsType): dataObject {
@@ -22,14 +23,19 @@ export default function extractInputData(inputView: powerbi.DataViewCategorical,
   scatter_cond = scatter_cond[0] === null ? rep(inputSettings.scatter, numerators.length) : scatter_cond;
   const highlights: powerbi.PrimitiveValue[] = inputView.values[0].highlights;
 
+  const inputValidStatus: string[] = validateInputData(keys, numerators, denominators, inputSettings.funnel.chart_type);
+
   const valid_ids: number[] = new Array<number>();
   const valid_keys: { x: number, id: number, label: string }[] = new Array<{ x: number, id: number, label: string }>();
+  const removalMessages: string[] = new Array<string>();
   let valid_x: number = 0;
-  for (let i: number = 0; i < denominators.length; i++) {
-    if (checkValidInput(numerators[i], denominators[i], inputSettings.funnel.chart_type)) {
+  for (let i: number = 0; i < numerators.length; i++) {
+    if (inputValidStatus[i] === "") {
       valid_ids.push(i);
       valid_keys.push({ x: valid_x, id: i, label: keys[i] })
       valid_x += 1;
+    } else {
+      removalMessages.push(`${keys[i]} removed due to: ${inputValidStatus[i]}.`)
     }
   }
 
@@ -42,6 +48,7 @@ export default function extractInputData(inputView: powerbi.DataViewCategorical,
     anyHighlights: highlights != null,
     percentLabels: (inputSettings.funnel.chart_type === "PR") && (inputSettings.funnel.multiplier === 1 || inputSettings.funnel.multiplier === 100),
     categories: inputView.categories[0],
-    scatter_formatting: extractValues(scatter_cond, valid_ids) as defaultSettingsType["scatter"][]
+    scatter_formatting: extractValues(scatter_cond, valid_ids) as defaultSettingsType["scatter"][],
+    warningMessage: removalMessages.length >0 ? removalMessages.join(" ") : ""
   }
 }
