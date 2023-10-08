079b8bb3c2a37e2dfb4bd1a193334f2a5dad10ee
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-10-08 12:50:51 +0300

    UAT fixes batch 2

--- src/Functions/extractDataColumn.ts
+++ src/Functions/extractDataColumn.ts
@@ -1,4 +1,5 @@
 import type powerbi from "powerbi-visuals-api"
+import formatPrimitiveValue from "./formatPrimitiveValue";
 type DataViewValueColumn = powerbi.DataViewValueColumn;
 type DataViewValueColumns = powerbi.DataViewValueColumns;
 type DataViewCategorical = powerbi.DataViewCategorical;
@@ -7,35 +8,51 @@ type VisualTooltipDataItem = powerbi.extensibility.VisualTooltipDataItem;
 
 type TargetT = number[] | string[] | number | string | VisualTooltipDataItem[][];
 
+function extractKeys(inputView: DataViewCategorical): string[] {
+  const primitiveKeyColumns = inputView.categories.filter(viewColumn => viewColumn.source?.roles?.["key"])
+
+  // If a 'Date Hierarchy' type is passed then there will be multiple 'key" entries
+  if (primitiveKeyColumns.length > 1) {
+    return primitiveKeyColumns[primitiveKeyColumns.length - 1].values.map((lastKeyValue: powerbi.PrimitiveValue, index) => {
+      if (lastKeyValue === null) {
+        return null
+      }
+      let concatKey: string = <string>lastKeyValue;
+      for (let i = (primitiveKeyColumns.length - 2); i >= 0; i--) {
+        concatKey += " " + primitiveKeyColumns[i].values[index];
+      }
+      return concatKey;
+    }) as string[];
+  } else {
+    const primitiveKeyValues = primitiveKeyColumns?.[0]?.values;
+    const primitiveKeyTypes = primitiveKeyColumns?.[0]?.source?.type;
+    return formatPrimitiveValue(primitiveKeyValues, primitiveKeyTypes)
+  }
+}
+
+function extractTooltips(inputView: DataViewCategorical): VisualTooltipDataItem[][] {
+  const tooltipColumns = inputView.values.filter(viewColumn => viewColumn.source.roles.tooltips);
+  return tooltipColumns?.[0]?.values?.map((_, idx) => {
+    return tooltipColumns.map(viewColumn => {
+      let tooltipValueFormatted: string = formatPrimitiveValue(viewColumn?.values?.[idx], viewColumn.source.type)
+
+      return <VisualTooltipDataItem>{
+        displayName: viewColumn.source.displayName,
+        value: tooltipValueFormatted
+      }
+    })
+  })
+}
+
 export default function extractDataColumn<T extends TargetT>(inputView: DataViewCategorical,
                                               name: string): T {
-  let columnRaw: DataViewValueColumn;
-  if (name === "key" || name === "group") {
-    const columnRawTmp: DataViewValueColumn[] = (inputView.categories as DataViewCategoryColumn[]).filter(viewColumn => {
-      return viewColumn.source.roles ? viewColumn.source.roles[name] : false;
-    });
-      columnRaw = columnRawTmp[0];
-      return <string[]>columnRaw.values as Extract<T, string[]>;
+  if (name === "key") {
+    return extractKeys(inputView) as Extract<T, string[]>;
   } else if (name === "tooltips") {
-    let rtn = new Array<VisualTooltipDataItem[]>();
-    const tooltipColumns = inputView.values.filter(viewColumn => viewColumn.source.roles.tooltips);
-    if (tooltipColumns.length > 0) {
-      rtn = tooltipColumns[0].values.map((_, idx) => {
-        return tooltipColumns.map(viewColumn => {
-          return <VisualTooltipDataItem>{
-            displayName: viewColumn.source.displayName,
-            value: viewColumn.source.type.numeric
-                    ? (<number>(viewColumn.values[idx])).toString()
-                    : <string>(viewColumn.values[idx])
-          }
-        })
-      })
-    }
-    return rtn as Extract<T, VisualTooltipDataItem[][]>;
-  }  else {
-    columnRaw = (inputView.values as DataViewValueColumns).filter(viewColumn => {
-      return viewColumn.source.roles ? viewColumn.source.roles[name] : false;
-    })[0];
-    return (columnRaw ? columnRaw.values : null) as T;
+    return extractTooltips(inputView) as Extract<T, VisualTooltipDataItem[][]>;
+  } else {
+    // Assumed that any other requested columns are numeric columns for plotting
+    const columnRaw = inputView.values.filter(viewColumn => viewColumn?.source?.roles?.[name]) as DataViewValueColumn[];
+    return columnRaw?.[0]?.values?.map(d => d === null ? null : Number(d)) as T
   }
 }
