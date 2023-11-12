bdfa2b06a972f74022dfd1f5c5da48b77d1c5371
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-11-12 13:47:13 +0000

    Fix outlier flagging with null limits

--- src/Outlier Flagging/three_sigma.ts
+++ src/Outlier Flagging/three_sigma.ts
@@ -2,9 +2,9 @@ import type { limitData } from "../Classes";
 
 export default function three_sigma(value: number,
                                     limits: limitData): string {
-  if (value < limits.ll99) {
+  if ((limits.ll99 !== null) && (value < limits.ll99)) {
     return "lower";
-  } else if (value > limits.ul99) {
+  } else if ((limits.ul99 !== null) && (value > limits.ul99)) {
     return "upper";
   } else {
     return "none";
--- src/Outlier Flagging/two_sigma.ts
+++ src/Outlier Flagging/two_sigma.ts
@@ -2,9 +2,9 @@ import type { limitData } from "../Classes";
 
 export default function two_sigma(value: number,
                    limits: limitData): string {
-  if (value < limits.ll99) {
+  if ((limits.ll95 !== null) && (value < limits.ll95)) {
     return "lower";
-  } else if (value > limits.ul99) {
+  } else if ((limits.ul95 !== null) && (value > limits.ul95)) {
     return "upper";
   } else {
     return "none";
