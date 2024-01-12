81540432375738ee285125f642ca6483dfa51715
Yixia Sun <sunyixia025@gmail.com>
2024-01-12 12:19:14 +1100

    add group label

--- src/D3 Plotting Functions/drawDots.ts
+++ src/D3 Plotting Functions/drawDots.ts
@@ -6,30 +6,42 @@ import { updateHighlighting } from "../D3 Plotting Functions";
 export default function drawDots(selection: svgBaseType, visualObj: Visual) {
   selection
     .select(".dotsgroup")
-    .selectAll("circle")
+    .selectAll("text") // Select text elements instead of circles
     .data(visualObj.viewModel.plotPoints)
-    .join("circle")
+    .join("text") // Create text elements
     .filter((d: plotData) => d.value !== null)
-    .attr("cy", (d: plotData) => visualObj.viewModel.plotProperties.yScale(d.value))
-    .attr("cx", (d: plotData) => visualObj.viewModel.plotProperties.xScale(d.x))
-    .attr("r", (d: plotData) => d.aesthetics.size)
+    .attr("y", (d: plotData) => visualObj.viewModel.plotProperties.yScale(d.value)) // Position on the y-axis
+    .attr("x", (d: plotData) => visualObj.viewModel.plotProperties.xScale(d.x)) // Position on the x-axis
+    .attr("dy", ".35em") // Adjust the vertical alignment of the text
+    .attr("text-anchor", "middle") // Center the text horizontally
     .style("fill", (d: plotData) => {
       const ylower: number = visualObj.viewModel.plotProperties.yAxis.lower;
       const yupper: number = visualObj.viewModel.plotProperties.yAxis.upper;
       const xlower: number = visualObj.viewModel.plotProperties.xAxis.lower;
       const xupper: number = visualObj.viewModel.plotProperties.xAxis.upper;
       return (between(d.value, ylower, yupper) && between(d.x, xlower, xupper))
-              ? d.aesthetics.colour
-              : "#FFFFFF";
+        ? d.aesthetics.colour
+        : "#FFF000";
     })
+    // .text((d: plotData) => {
+    //   return "3"
+    // })
+    //.text((d: plotData) => {console.log("Key:", d.x)})
+    .text((d: plotData) => {
+      return d.tooltip.find((element) => element.displayName == "Group").value
+    })
+
+
+
+
     .on("click", (event, d: plotData) => {
       // Pass identities of selected data back to PowerBI
       visualObj.selectionManager
-          // Propagate identities of selected data back to
-          //   PowerBI based on all selected dots
-          .select(d.identity, (event.ctrlKey || event.metaKey))
-          // Change opacity of non-selected dots
-          .then(() => { selection.call(updateHighlighting, visualObj); });
+        // Propagate identities of selected data back to
+        //   PowerBI based on all selected dots
+        .select(d.identity, (event.ctrlKey || event.metaKey))
+        // Change opacity of non-selected dots
+        .then(() => { selection.call(updateHighlighting, visualObj); });
 
       event.stopPropagation();
     })
