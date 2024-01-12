43d8c83bf77323b526990b9de60ab86ce560b32d
pli9 <pli@westswan.com>
2024-01-12 14:29:03 +0800

    Set scatter dots layer on top of tooltip lines

--- src/visual.ts
+++ src/visual.ts
@@ -91,9 +91,9 @@ export class Visual implements powerbi.extensibility.IVisual {
       this.svg.selectChildren().remove();
     }
     this.svg.append('g').classed("linesgroup", true)
-    this.svg.append('g').classed("dotsgroup", true)
     this.svg.append('line').classed("ttip-line-x", true)
     this.svg.append('line').classed("ttip-line-y", true)
+    this.svg.append('g').classed("dotsgroup", true)
     this.svg.append('g').classed("xaxisgroup", true)
     this.svg.append('text').classed("xaxislabel", true)
     this.svg.append('g').classed("yaxisgroup", true)
