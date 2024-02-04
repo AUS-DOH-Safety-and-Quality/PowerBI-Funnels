ea4c4353bbf165b0e28f8226b4d35a469d15dee7
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-02-04 20:00:53 +0200

    Stray loggin

--- src/visual.ts
+++ src/visual.ts
@@ -40,7 +40,6 @@ export class Visual implements powerbi.extensibility.IVisual {
       this.viewModel.inputSettings.update(options.dataViews[0]);
       validateDataView(options.dataViews);
       this.viewModel.update(options, this.host);
-      console.log(this.viewModel)
 
       this.svg.call(drawXAxis, this)
               .call(drawYAxis, this)
