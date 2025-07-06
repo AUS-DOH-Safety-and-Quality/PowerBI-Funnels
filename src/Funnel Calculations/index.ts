963f96bdb07697564bfed6b773f5a2d6553e0ac6
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-07-06 19:28:11 +0800

    Initial infra for R front-end

---
+++ src/Funnel Calculations/index.ts
@@ -0,0 +1,5 @@
+export { default as getPhi } from "./getPhi"
+export { default as getTau2 } from "./getTau2"
+export { default as getTransformation } from "./getTransformation"
+export { default as getZScores } from "./getZScores"
+export { default as winsoriseZScores } from "./winsoriseZScores"
---
+++ src/Outlier Flagging/index.ts
@@ -0,0 +1,2 @@
+export { default as three_sigma } from "./three_sigma";
+export { default as two_sigma } from "./two_sigma";
