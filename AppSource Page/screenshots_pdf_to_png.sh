d37641205d4e629c98bd3445a19fc184e7e9c880
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-08-11 10:54:59 +0800

    Add new screenshots and process

---
+++ AppSource Page/screenshots_pdf_to_png.sh
@@ -0,0 +1,4 @@
+# First export the 'Screenshots.pbix' file to PDF
+# The below will create a separate "screenshot-*.png" file for each
+#   page in the .pbix
+pdftoppm Screenshots.pdf screenshot -png -r 300 -scale-to-x 1366 -scale-to-y 768
