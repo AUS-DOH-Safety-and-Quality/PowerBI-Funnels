0abec7b7c2955a6f4543c110e474ffcc9b06d60e
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2026-01-10 19:27:09 +0800

    Remove stdlib chisquare

--- src/Functions/qchisq.ts
+++ src/Functions/qchisq.ts
@@ -1,5 +1,5 @@
 import qgamma from "./qgamma";
 
-export default function qchisq(p: number, df: number, lower_tail: boolean, log_p: boolean): number {
+export default function qchisq(p: number, df: number, lower_tail: boolean = true, log_p: boolean = false): number {
   return qgamma(p, 0.5 * df, 2.0, lower_tail, log_p);
 }
